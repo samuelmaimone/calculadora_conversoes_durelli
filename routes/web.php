3d39f321473050a8c0c609cdea02312a8b25cce1
samuelmaimone <samuelmaimone55@gmail.com>
2026-05-26 17:17:21 -0300

    commit inicial: f1 a f5 e estilizações finalizadas

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::post('/converter', [calculadoraController::class, 'converterBin']);
 Route::post('/converter-para-decimal', [calculadoraController::class, 'converterParaDecimal']);
 Route::post('/converter-f3', [calculadoraController::class, 'converterAgrupamento']);
 Route::post('/converter-f4', [calculadoraController::class, 'converterIntermediario']);
+
+;
